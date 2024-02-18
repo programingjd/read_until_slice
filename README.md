00c1d4b36f7aa79bcb7e53311f518eb4736eba65
programingjd <programingjd@gmail.com>
2024-02-18 15:57:28 +0100

    README and example.

---
+++ README.md
@@ -0,0 +1,43 @@
+## read_until_slice &nbsp;[![LICENSE](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE) [![crates.io Version](https://img.shields.io/crates/v/read_until_slice.svg)](https://crates.io/crates/read_until_slice) [![Documentation](https://docs.rs/read_until_slice/badge.svg)](https://docs.rs/read_until_slice)
+
+The tokio io-util feature provides the method:
+```rust
+pub async fn read_until(&mut self, delimiter: u8, buf: Vec<u8>) -> Result<usize>
+```
+on `impl AsyncBufRead + Unpin`.
+
+This reads from an async buffered reader until either EOF or the delimiter is reached.
+
+While useful, it is limited to a single byte delimiter.
+
+This crate extends this by taking a slice as a delimiter instead of a single byte.
+
+```rust
+pub async fn read_until(&mut self, delimiter: u8, buf: Vec<u8>) -> Result<usize>
+```
+on the same `impl AsyncBufRead + Unpin`.
+
+Example
+
+```rust
+// Open socket
+let stream = TcpStream::connect(addr)
+    .await
+    .expect("could not connect to remote address");
+// Split stream into reader and writer halves
+let (reader, mut writer) = split(stream);
+// Manual HTTP/1.1 request for /status/200
+let mut reader = BufReader::new(reader);
+...
+let mut buffer = vec![];
+let delimiter = b"\r\n";
+let n = reader.read_until(delimiter, &mut buffer)
+    .await
+    .expect("could not read from socket");
+assert_eq!(n, buffer.len());
+if buffer.ends_with(delimiter) {
+    println!("end of line delimiter reached");
+} else {
+    println!("end of stream reached");
+}
+```
---
+++ examples/http.rs
@@ -0,0 +1,69 @@
+use read_until_slice::AsyncBufReadUntilSliceExt;
+use std::str::from_utf8;
+use tokio::io::{copy, sink, split, AsyncWriteExt, BufReader};
+use tokio::join;
+use tokio::net::{lookup_host, TcpStream};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // DNS lookup for http://httpbingo.org
+    let addr = lookup_host("httpbingo.org:80")
+        .await
+        .ok()
+        .and_then(|mut it| it.next())
+        .unwrap();
+    // Open socket
+    let stream = TcpStream::connect(addr).await.unwrap();
+    // Split stream into reader and writer halves
+    let (reader, mut writer) = split(stream);
+    // Manual HTTP/1.1 request for /status/200
+    let mut reader = BufReader::new(reader);
+    let request = b"\
+        GET /status/200 HTTP/1.1\r\n\
+        Host: httpbingo.org\r\n\
+        Connection: close\r\n\
+        User-Agent: crate/example/read_until_slice\r\n\
+    \r\n";
+    println!("{}", from_utf8(request).unwrap());
+    // Write request and read response
+    join!(
+        // Write request
+        async move {
+            writer.write_all(request).await.unwrap();
+        },
+        // Read response
+        async {
+            // Read status line
+            let mut buffer = vec![];
+            let _ = reader.read_until_slice(b"\r\n", &mut buffer).await.unwrap();
+            // If buffer doesn't end with \r\n, it means we exhausted the reader
+            // before reaching our delimiter
+            assert!(buffer.ends_with(b"\r\n"));
+            let status_line = &buffer[..buffer.len() - 2];
+            println!("{}", status_line.escape_ascii().to_string());
+
+            let mut split = status_line.split(|&b| b == b' ');
+            let http_version = split.next().unwrap();
+            assert_eq!(http_version, b"HTTP/1.1");
+            let status_code = split.next().unwrap();
+            assert_eq!(status_code, b"200");
+            let status_text = split.next().unwrap();
+            assert_eq!(status_text, b"OK");
+            // Read headers
+            loop {
+                buffer.clear();
+                let _ = reader.read_until_slice(b"\r\n", &mut buffer).await.unwrap();
+                // If buffer doesn't end with \r\n, it means we exhausted the reader
+                // before reaching our delimiter
+                assert!(buffer.ends_with(b"\r\n"));
+                let header_line = &buffer[..buffer.len() - 2];
+                println!("{}", header_line.escape_ascii().to_string());
+                if header_line.is_empty() {
+                    break;
+                };
+            }
+            // Discard body if there's one
+            copy(&mut reader, &mut sink()).await.unwrap();
+        }
+    );
+}
